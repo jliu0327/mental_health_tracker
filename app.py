3c1c9572900d0b027e38857c09c6b207bf60c41c
jliu0327 <justinliu90@gmail.com>
2025-05-29 00:40:04 -0400

    updated log in page that will be used as template

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, redirect, render_template, request, session
 import sqlite3
 from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
@@ -20,3 +21,6 @@ def db_connection():
     conn = sqlite3.connect('mental_health.db')
     return conn
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
